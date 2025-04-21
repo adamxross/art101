f8434017e4060dcf80ca5b23be23f6a98a13ff27
adamxross <adamross03.a@gmail.com>
2025-04-21 12:34:54 -0700

    Lab 4 Changes

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Rock Paper Scissors Pseudocode
+// Author: Adam Ross
+// Date: April 2025
+
+// Start the game with title ‘Rock, Paper, Scissors’
+  // Define the three possible choices: rock, paper, and scissors
+    // Establish Scissors beats Paper, Paper beats Rock, Rock beats Scissors, and Ties.
+// Ask the player to choose rock, paper, or scissors (or type 'quit' to end the game)
+	//Display user choice 
+// Randomly select one of the options for the computer
+	// Display computer choice between Rock/Paper/Scissors
+		// Paste one of nine possible outcomes
+		  // Allow for computer to tie with player
+// If the input is not one of the valid options, inform the player and restart the game
+// Compare the player’s choice and the computer’s choice:
+    // If both are the same, it's a tie
+    // If the player beats the computer (rock beats scissors, paper beats rock, scissors beats paper), the player wins
+        // In all other cases, the computer wins
+          // If player wins, add +1 to player counter
+          // If computer wins, add +1 to computer counter
+// Loop to keep the game running until the player wants to quit
+  // If the player types 'quit', exit the loop and end the game
+    // After exiting the loop, shut down the game
 
 // Constants
 
